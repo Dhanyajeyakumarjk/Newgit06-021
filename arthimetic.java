c3bfbca576e8616cb8cfe765d720682ca2b472ca
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-08 14:13:39 +0530

    fff

--- arthimetic.java
+++ arthimetic.java
@@ -2,7 +2,7 @@ public class arthimetic{
     public static void main(String[] args){
         int a=10;
         int b=20;
-        int x=a*b;
+        int x=a+b;
 
         System.out.println("Sum of two numbers:"+x);
         
